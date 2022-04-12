d296f7962d680767e5b5b45766fa6f714fa1bf51
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-04-12 14:16:33 -0300

    feat: change fonts and restyling the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import {Topbar} from './components/Topbar'
-
 function App() {
   return (
     <div className="App">
-      <Topbar />
+      <div>Dashboard Intratec tecnologia</div>
     </div>
   );
 }
