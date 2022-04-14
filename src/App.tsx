a4606965f39d7e3f2385d88e9b6eb174a6faea73
Valdir Silva <valdir.ti@gmail.com>
2022-04-13 23:30:22 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { Home } from "./Home";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import { Home } from './Home'
+import { List } from './List'
+import { Login } from './Login'
+import { New } from './New'
+import { Single } from './Single'
 
 function App() {
   return (
     <div className="App">
-      <div>Dashboard Intratec tecnologia</div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />}/>
+            <Route path="users">
+              <Route index element={<List />}/>
+              <Route path=':userId' element={<Single />}/>
+              <Route path='new' element={<New />}/>
+            </Route>
+            <Route path="products">
+              <Route index element={<List />}/>
+              <Route path=':productId' element={<Single />}/>
+              <Route path='new' element={<New />}/>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
