a13905a2a4e3e41219b932547731f1802a6f87a0
Valdir Silva <valdir.ti@gmail.com>
2022-04-17 23:18:31 -0300

    refact: remove commas

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,10 +1,10 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
 import './styles.scss'
 
@@ -61,7 +61,7 @@ function List() {
             method: "Online",
             status: "Pending",
         },
-    ];
+    ]
 
     return (
         <TableContainer component={Paper} className='table'>
--- src/pages/New/index.tsx
+++ src/pages/New/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 import { DriveFolderUploadOutlined } from '@mui/icons-material'
 
