fe1f2b1a1b3e26e61ae5bc6a91f50d6d5cac19e8
Valdir Silva <valdir.ti@gmail.com>
2022-04-18 02:11:27 -0300

    feat: firebase login with redirect

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
+import { auth } from '../../firebase'
+
+import './styles.scss'
+
 function Login() {
+
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleLogin = (e: any) => {
+    e.preventDefault()
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user
+        navigate("/")
+      }).catch((err) => {
+        setError(true)
+        console.log(err)
+      })
+  }
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+        <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
+        <button type='submit'>Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
   )
 }
 
-export {Login}
+export { Login }
